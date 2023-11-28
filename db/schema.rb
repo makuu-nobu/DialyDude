ff0bdb805ec4515dfd2f818e8cd060c9a6b27a83
makuu-nobu <nobuatsu.ltnl@icloud.com>
2023-11-28 16:43:56 +0900

    詳細画面作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_28_045937) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_28_074050) do
   create_table "active_storage_attachments", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -37,6 +37,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_28_045937) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "counts", charset: "utf8mb4", force: :cascade do |t|
+    t.bigint "post_id", null: false
+    t.integer "count", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_counts_on_post_id"
   end
 
   create_table "posts", charset: "utf8mb4", force: :cascade do |t|
@@ -67,5 +75,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_28_045937) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "counts", "posts"
   add_foreign_key "posts", "users"
 end
