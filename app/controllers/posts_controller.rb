067ea7fa0e11261d73bcc0c7332913f3bc1d93bf
makuu-nobu <nobuatsu.ltnl@icloud.com>
2023-11-28 17:36:03 +0900

    作成時に閲覧数の設定と詳細時にカウントが上がるように設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,24 +4,38 @@ class PostsController < ApplicationController
     end
 
     def new
-        @post = Post.new        
+        @post = Post.new   
     end
 
     def create
-        post = Post.create(post_params)
-        if post.save
-            redirect_to "/"            
+        @post = Post.new(post_params)
+        if @post.save
+            count_build(@post)
+            redirect_to root_path           
         else
-            render :new            
+            render :new, status: :unprocessable_entity      
         end        
     end
 
     def show
         @post = Post.find(params[:id])
+        update_view_count(@post)
+
     end
 
     private
     def post_params
         params.require(:post).permit(:title, :subtitle, :content, :image, :category_id, :month_id).merge(user_id: current_user.id)
+    end
+
+    def update_view_count(post)
+        count = post.count || post.build_count
+        count.increment!(:count)
+        #integer型の保有制限は256なので、のちに超過分を別カラムに記録する処理を考える
+    end
+
+    def count_build(post)
+        @count = Count.new(post_id: post.id)
+        @count.save
     end
 end
