8eb1407d6872c0a85e08aff9d11c574cf5aedc66
makuu-nobu <nobuatsu.ltnl@icloud.com>
2024-01-23 13:11:32 +0900

    軽微な変更

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,7 +3,8 @@ class Category < ActiveHash::Base
         { id: 1, name: '---' },
         { id: 2, name: '趣味' },
         { id: 3, name: '雑談' },
-        { id: 4, name: 'プログラミング' }
+        { id: 4, name: 'プログラミング' },
+        { id: 5, name: 'テストメッセージ' }
     ]
 
     include ActiveHash::Associations
